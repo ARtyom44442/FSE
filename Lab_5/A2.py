306a039a7c01187b4d522df3f4e49030caee11fb
Tereshko Artyom <Tereshko_AP_25@student.grsu.by>
2025-11-16 17:30:28 +0300

    Добавил цикл для счета предложений и вывода каждого

--- Lab_5/A2.py
+++ Lab_5/A2.py
@@ -1,3 +1,12 @@
 import re
 text = "He jests at scars. That never felt a wound! Hello, friend! Are you OK?"
 sentences = re.split(r'(?<=[.?!]) ', text)
+num = 0
+
+for sentence in sentences:
+    num += 1
+    print(sentence)
+
+
+print("Количество предложение равно", num)
+
