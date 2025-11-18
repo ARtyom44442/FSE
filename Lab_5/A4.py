d2e0d7708e013f71cf2aa8f3d595abe2da4f8756
Tereshko Artyom <Tereshko_AP_25@student.grsu.by>
2025-11-18 19:21:16 +0300

    Добавил функции поиска и различий белков

--- Lab_5/A4.py
+++ Lab_5/A4.py
@@ -11,3 +11,26 @@ def decode_rle(sequence):
             decoded.append(sequence[i])
             i += 1
     return ''.join(decoded)
+
+
+def search(seq, endcoded_seq):
+    decoded = decode_rle(endcoded_seq)
+    if seq in decoded:
+        return True
+    else:
+        return False
+
+def diff(first_chain, second_chain):
+    decoded_first = decode_rle(first_chain)
+    decoded_second = decode_rle(second_chain)
+
+    difference_count =abs(len(decoded_first) - len(decoded_second))
+
+    max_len = max(len(decoded_first), len(decoded_second))
+    for i in range(max_len):
+        char_first = decoded_first[i] if i < len(decoded_first) else None
+        char_second = decoded_second[i] if i < len(decoded_second) else None
+        if char_first != char_second:
+            difference_count += 1
+
+    return difference_count
